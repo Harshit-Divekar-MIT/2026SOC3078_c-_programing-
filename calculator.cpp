4a23c1d53c73accc164e9c20f55cff6724747306
Harshit-Divekar-MIT <harshitdivekar2908@gmail.com>
2026-09-04 17:25:23 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b , sum=0;
+    cout<<"enter the number";  
+    cin>>a>>b;
+    cout<<"1.additioin\n
+    2.subtraction\n
+    3.multiplication\n
+    4.division\n"
+    cin>>ch;
+    switch (ch) 
+
+    {
+    case 1:
+        s = a + b ,
+        cout << "Addition is = "<<S,
+        case 2 :
+        s = a - b ;
+        cout << "sub"
+    }
+    
